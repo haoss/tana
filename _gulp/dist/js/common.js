f7de97e8224108212fe7e8ea104ef94e69d5cac4
DESKTOP-URN9BNQ\haoss <haoss_ut@mail.ru>
2017-08-01 17:09:23 +0300

    no message

--- _gulp/dist/js/common.js
+++ _gulp/dist/js/common.js
@@ -79,8 +79,6 @@ $(document).on('ready', function(){
     midClick: true // Allow opening popup on middle mouse click. Always set it to true if you don't provide alternative source in href.
   });
 
-  console.log('ready');
-
   // Chrome Smooth Scroll
   try {
     $.browserSelector();
@@ -98,7 +96,6 @@ $(document).on('ready', function(){
 $(window).on('load', function() {
   // $(".loader_inner").fadeOut();
   $(".loader").delay(400).fadeOut("slow");
-  console.log('load');
 });
 
 /*
